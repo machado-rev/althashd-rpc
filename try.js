66b612b51259ba7f55f732af07c363d1e5d341a4
Leandro Machado <sr.machado@gmail.com>
2020-09-15 14:40:43 -0300

    commonjs

--- try.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var althashd_rpc_1 = require("./node_modules/althashd-rpc");
-var rpc = new althashd_rpc_1["default"]('http://localhost:4889');
-var balance = await rpc.getBalance();
-console.log(balance);
--- try.ts
+++
@@ -1,6 +0,0 @@
-import AlthashRpc from 'althashd-rpc';
-
-const rpc = new AlthashRpc('http://localhost:4889');
-
-const balance = await rpc.getBalance();
-console.log(balance);
